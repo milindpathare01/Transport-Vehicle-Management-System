b7a78522b28bb1c5aa18e18ff164e09629220cf9
milind01 <milindpathare93001@gmail.com>
2022-11-25 10:48:57 +0530

    Source Code

---
+++ project/src/ConnectionProvider.java
@@ -0,0 +1,31 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author milin
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/milind?autoRecoonect=true&useSSL=false","root","");
+            return con;
+        }
+        catch(Exception e)
+        {
+         return null;   
+        }
+    
+    }
+    
+}
